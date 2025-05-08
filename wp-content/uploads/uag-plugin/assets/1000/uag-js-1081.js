39b99ca24c198ea76aeeb6b3975fa19fec353cd8
MIJANUR RAHMAN <mijanurrahman@Mijanurs-MacBook-Pro-2.local>
2025-05-08 21:58:20 +0300

    replaced picture

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1081.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-b7c6b712' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-67d678b0' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-47da3253' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-7464faea' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-3886ae12' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-bd34043a' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-aa699691' );
+});
+ });
