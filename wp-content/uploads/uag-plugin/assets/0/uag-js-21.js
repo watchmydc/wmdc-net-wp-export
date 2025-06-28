78dc86abb00a6649ded55b636b738d9800938013
MIJANUR RAHMAN <mijanurrahman@Mijanurs-MacBook-Pro-2.local>
2025-06-28 11:31:49 +0300

    modified blog homepage

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-21.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-0718c591' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-2ef800c6' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-b789e3b6' );
+});
+ });
