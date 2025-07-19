ca942d3d6de5d7b6484ba308b63d34c92e69e4fc
MIJANUR RAHMAN <mijanurrahman@Mijanurs-MacBook-Pro-2.local>
2025-07-19 13:37:29 +0300

    more articles

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1423.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-e5df8ed2' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-4c41ac28' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-62bd5cdc' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-fbcc79a6' );
+});
+ });
