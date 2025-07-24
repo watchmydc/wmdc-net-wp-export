096548f1b1cfb9e422ed0ac1ed6d435d26a72acd
MIJANUR RAHMAN <mijanurrahman@Mijanurs-MacBook-Pro-2.local>
2025-07-24 22:35:18 +0300

    updated sitemap

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1450.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-7c8605f2' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-eada8452' );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1470.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-eada8452' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-6ea38907' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-eada8452' );
+});
+ });
