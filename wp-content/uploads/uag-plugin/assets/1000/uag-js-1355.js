c670d467d12e30b76c6e6712c77f23b294c80685
MIJANUR RAHMAN <mijanurrahman@Mijanurs-MacBook-Pro-2.local>
2025-08-22 13:35:46 +0300

    TL how-to guide banner, cta

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1355.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-a5db9177' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-6f04ef67' );
+});
+ });
